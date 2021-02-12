4895efd4a395b2404ee742eb9ae496111f19872a
Cameron Collins <cameroncollins19@yahoo.com>
2021-02-11 18:38:59 -0600

    Added File input and output

--- FStreamExample.cpp
+++ FStreamExample.cpp
@@ -1,20 +1,43 @@
 // FStreamExample.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+/* FStream: 
+	-ofstream: write to files
+	-ifstream: read from files
+	-fstream: read and write from files
+*/
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//Variables
+	const std::string directory = "example.txt";
+	std::string line = "";
+
+	//Create a File and Output to it
+	std::ofstream w_file;
+	w_file.open(directory);
+	w_file << "File Output!\n";
+	w_file.close();
+
+	//Read From Created File
+	std::ifstream r_file;
+	r_file.open(directory);
+
+	if (r_file.is_open()) {
+		while (std::getline(r_file, line)) {
+			std::cout << line << '\n';
+		}
+
+		r_file.close();
+	}
+	else {
+		std::cout << "Unable to open file";
+	}
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
