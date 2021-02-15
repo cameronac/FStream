7eb1c609620fa4bec8580a450c9bfef9b2c37eb6
Cameron Collins <cameroncollins19@yahoo.com>
2021-02-15 17:48:18 -0600

    Added .txt and Opened File

--- IntermediateFileReading/IntermediateFileReading.cpp
+++ IntermediateFileReading/IntermediateFileReading.cpp
@@ -2,19 +2,32 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//Variables
+	std::string directory = "100west.txt";
+	std::string line = "";
+
+	//Input File
+	std::ifstream myFile;
+	myFile.open(directory);
+
+	//Getting File Text
+	if (myFile.is_open()) {
+
+		//File Loop
+		while (std::getline(myFile, line)) {
+
+			//Process Line
+			
+		}
+	}
+	else {
+		std::cout << "Unable to open file!" << std::endl;
+	}
+
+	myFile.close();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
